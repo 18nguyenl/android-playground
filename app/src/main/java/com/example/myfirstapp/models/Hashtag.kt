75d3bffe2dc9e85a1655d064377048334245e649
ambitiousmustard <ambitiousmustard.gmail.com>
2020-06-12 20:23:01 -0500

    Gave hashtag model basic infrastructure for database

--- app/src/main/java/com/example/myfirstapp/models/Hashtag.kt
+++ app/src/main/java/com/example/myfirstapp/models/Hashtag.kt
@@ -1,7 +1,16 @@
 package com.example.myfirstapp.models
 
-class Hashtag {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "hashtags")
+class Hashtag(@ColumnInfo(name = "name") val name: String) {
 
+    @PrimaryKey(autoGenerate = true) var tid: Int = 0
+
+    override fun toString(): String {
+        return "#$name"
+    }
 
 }
